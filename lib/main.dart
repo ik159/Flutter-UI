3e5a9098d5022a7f04bd4aa4ff39db662b5fa688
ik159 <55886905+ik159@users.noreply.github.com>
2020-01-26 11:52:29 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -220,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         children: <Widget>[
                           Icon(
                             Icons.access_time,
-                            color: Colors.white,
+                            color: Colors.black,
                             size: 30.0,
                           ),
                           Container(
